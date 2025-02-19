c64e85e3578978ecc420096cda17d98447b63c55
Nolij <xdmatthewbx@mixhosting.xyz>
2025-02-18 23:21:10 -0500

    RecipeManager improvements (memory should hopefully actually be better than JEMI now)

---
+++ src/main/java/dev/nolij/toomanyrecipeviewers/impl/api/gui/builder/RecipeLayoutBuilder.java
@@ -0,0 +1,69 @@
+package dev.nolij.toomanyrecipeviewers.impl.api.gui.builder;
+
+import mezz.jei.api.gui.builder.IIngredientAcceptor;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.builder.IRecipeSlotBuilder;
+import mezz.jei.api.ingredients.ITypedIngredient;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.runtime.IIngredientManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class RecipeLayoutBuilder implements IRecipeLayoutBuilder {
+	
+	private final IIngredientManager ingredientManager;
+	
+	public boolean shapeless = false;
+	
+	public final List<ITypedIngredient<?>> inputs = new ArrayList<>();
+	public final List<ITypedIngredient<?>> catalysts = new ArrayList<>();
+	public final List<ITypedIngredient<?>> outputs = new ArrayList<>();
+	
+	public RecipeLayoutBuilder(IIngredientManager ingredientManager) {
+		this.ingredientManager = ingredientManager;
+	}
+	
+	@Override
+	public IRecipeSlotBuilder addSlot(RecipeIngredientRole role) {
+		return new RecipeSlotBuilder(this, ingredientManager, role);
+	}
+	
+	@SuppressWarnings("removal")
+	@Override
+	public IRecipeSlotBuilder addSlotToWidget(RecipeIngredientRole role, mezz.jei.api.gui.widgets.ISlottedWidgetFactory<?> widgetFactory) {
+		return new RecipeSlotBuilder(this, ingredientManager, role);
+	}
+	
+	@Override
+	public IIngredientAcceptor<?> addInvisibleIngredients(RecipeIngredientRole role) {
+		return new RecipeSlotBuilder(this, ingredientManager, role);
+	}
+	
+	@Override
+	public void moveRecipeTransferButton(int x, int y) {
+		
+	}
+	
+	@Override
+	public void setShapeless() {
+		shapeless = true;
+	}
+	
+	@Override
+	public void setShapeless(int x, int y) {
+		shapeless = true;
+	}
+	
+	@Override
+	public void createFocusLink(IIngredientAcceptor<?>... iIngredientAcceptors) {
+		
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(inputs, catalysts, outputs);
+	}
+	
+}

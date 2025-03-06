acc8aed9a3acd327bdef570b00058954a34542b6
Nolij <xdmatthewbx@mixhosting.xyz>
2025-03-05 22:21:11 -0500

    replace `@Overwrite` with `@Inject` with `require = 0` so Create can fix the issue without causing TMRV errors

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,1 @@
-- fixed Create recipes reporting incorrect fluid amounts
 - further improvements to overall system stability and other minor adjustments have been made to enhance the user experience
--- src/main/java/dev/nolij/toomanyrecipeviewers/mixin/CreateRecipeCategoryMixin.java
+++ src/main/java/dev/nolij/toomanyrecipeviewers/mixin/CreateRecipeCategoryMixin.java
@@ -3,18 +3,16 @@ package dev.nolij.toomanyrecipeviewers.mixin;
 import com.simibubi.create.compat.jei.category.CreateRecipeCategory;
 import net.neoforged.neoforge.fluids.FluidStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = CreateRecipeCategory.class, remap = false)
 public class CreateRecipeCategoryMixin {
 	
-	/**
-	 * @author	Nolij
-	 * @reason	Create is dumb
-	 */
-	@Overwrite
-	public static FluidStack withImprovedVisibility(FluidStack stack) {
-		return stack.copy();
+	@Inject(method = "withImprovedVisibility(Lnet/neoforged/neoforge/fluids/FluidStack;)Lnet/neoforged/neoforge/fluids/FluidStack;", at = @At("HEAD"), cancellable = true, require = 0)
+	private static void tmrv$withImprovedVisibility$HEAD(FluidStack stack, CallbackInfoReturnable<FluidStack> cir) {
+		cir.setReturnValue(stack.copy());
 	}
 	
 }

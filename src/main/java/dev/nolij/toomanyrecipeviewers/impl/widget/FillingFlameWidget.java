41ff3a45645e1d650d3c9fce8e1c87a695ae46b0
Evan Bowness <bownessevan@gmail.com>
2026-06-23 13:35:40 -0700

    Fix recipe flame and arrow animation speed and flame direction (#72)
    * fix animated arrow/flame cook time units and flame widget texture coordinates
    
    * fix: arrow tweaks
    
    * fix: switch to native EMI textures

--- src/main/java/dev/nolij/toomanyrecipeviewers/impl/widget/FillingFlameWidget.java
+++ src/main/java/dev/nolij/toomanyrecipeviewers/impl/widget/FillingFlameWidget.java
@@ -1,6 +1,6 @@
 package dev.nolij.toomanyrecipeviewers.impl.widget;
 
-import dev.emi.emi.EmiRenderHelper;
+import dev.emi.emi.api.render.EmiTexture;
 import dev.emi.emi.api.widget.AnimatedTextureWidget;
 import dev.emi.emi.runtime.EmiDrawContext;
 import net.minecraft.client.gui.GuiGraphics;
@@ -8,14 +8,24 @@ import net.minecraft.client.gui.GuiGraphics;
 public class FillingFlameWidget extends AnimatedTextureWidget {
 	
 	public FillingFlameWidget(int x, int y, int time) {
-		super(EmiRenderHelper.WIDGETS, x, y, 14, 14, 68, 14, time, true, false, false);
+		super(EmiTexture.FULL_FLAME.texture, x, y,
+				EmiTexture.FULL_FLAME.width, EmiTexture.FULL_FLAME.height,
+				EmiTexture.FULL_FLAME.u, EmiTexture.FULL_FLAME.v,
+				EmiTexture.FULL_FLAME.regionWidth, EmiTexture.FULL_FLAME.regionHeight,
+				EmiTexture.FULL_FLAME.textureWidth, EmiTexture.FULL_FLAME.textureHeight,
+				time, false, true, true);
 	}
 	
 	@Override
 	public void render(GuiGraphics draw, int mouseX, int mouseY, float delta) {
 		EmiDrawContext context = EmiDrawContext.wrap(draw);
-		context.drawTexture(this.texture, x, y, width, height, u, 0, regionWidth, regionHeight, textureWidth, textureHeight);
+
+		context.drawTexture(EmiTexture.EMPTY_FLAME.texture, x, y,
+				EmiTexture.EMPTY_FLAME.width, EmiTexture.EMPTY_FLAME.height,
+				EmiTexture.EMPTY_FLAME.u, EmiTexture.EMPTY_FLAME.v,
+				EmiTexture.EMPTY_FLAME.regionWidth, EmiTexture.EMPTY_FLAME.regionHeight,
+				EmiTexture.EMPTY_FLAME.textureWidth, EmiTexture.EMPTY_FLAME.textureHeight);
+
 		super.render(context.raw(), mouseX, mouseY, delta);
 	}
-	
 }
